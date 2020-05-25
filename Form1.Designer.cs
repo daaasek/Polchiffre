302b12e8b1c564a3e5129ce48bb5838e3155468c
daaasek <daaasek@gmail.com>
2020-05-25 15:23:53 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.oNasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.twórcyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stronaWWWToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.markAll = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 18);
+            this.textBox1.MinimumSize = new System.Drawing.Size(100, 100);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(255, 100);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(267, 122);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tekst  do przetworzenia";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(288, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(252, 209);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Wynik działania programu";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 18);
+            this.textBox2.MinimumSize = new System.Drawing.Size(100, 100);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(240, 185);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Text = "Automatycznie zapisuje wynik w schowku użytkownika";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton2);
+            this.groupBox3.Controls.Add(this.radioButton1);
+            this.groupBox3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox3.Location = new System.Drawing.Point(18, 155);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(218, 43);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Wybierz, co mogę dla ciebie zrobić";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(79, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(66, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "odszyfruj";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(8, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(65, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "zaszyfruj";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Uruchom narzędzie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.markAll);
+            this.groupBox4.Controls.Add(this.checkBox6);
+            this.groupBox4.Controls.Add(this.checkBox5);
+            this.groupBox4.Controls.Add(this.checkBox4);
+            this.groupBox4.Controls.Add(this.checkBox3);
+            this.groupBox4.Controls.Add(this.checkBox2);
+            this.groupBox4.Controls.Add(this.checkBox1);
+            this.groupBox4.Location = new System.Drawing.Point(18, 204);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(218, 96);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Używając klucza: ";
+            this.groupBox4.Enter += new System.EventHandler(this.groupBox4_Enter);
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(140, 42);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(62, 17);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.Text = "Dastina";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(140, 19);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(72, 17);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Mateusza";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(77, 42);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(50, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Kuby";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(77, 19);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(57, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Pawła";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(4, 42);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(61, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Gabrieli";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(4, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(67, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Łukasza";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oNasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(540, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // oNasToolStripMenuItem
+            // 
+            this.oNasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.twórcyToolStripMenuItem,
+            this.stronaWWWToolStripMenuItem});
+            this.oNasToolStripMenuItem.Name = "oNasToolStripMenuItem";
+            this.oNasToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.oNasToolStripMenuItem.Text = "O nas";
+            // 
+            // twórcyToolStripMenuItem
+            // 
+            this.twórcyToolStripMenuItem.Name = "twórcyToolStripMenuItem";
+            this.twórcyToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.twórcyToolStripMenuItem.Text = "Twórcy";
+            this.twórcyToolStripMenuItem.Click += new System.EventHandler(this.twórcyToolStripMenuItem_Click);
+            // 
+            // stronaWWWToolStripMenuItem
+            // 
+            this.stronaWWWToolStripMenuItem.Name = "stronaWWWToolStripMenuItem";
+            this.stronaWWWToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.stronaWWWToolStripMenuItem.Text = "Strona WWW";
+            this.stronaWWWToolStripMenuItem.Click += new System.EventHandler(this.stronaWWWToolStripMenuItem_Click);
+            // 
+            // markAll
+            // 
+            this.markAll.Location = new System.Drawing.Point(6, 65);
+            this.markAll.Name = "markAll";
+            this.markAll.Size = new System.Drawing.Size(206, 25);
+            this.markAll.TabIndex = 8;
+            this.markAll.Text = "Zaznacz wszystkich";
+            this.markAll.UseVisualStyleBackColor = true;
+            this.markAll.Click += new System.EventHandler(this.markAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 306);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Polchiffre";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem oNasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem twórcyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stronaWWWToolStripMenuItem;
+        private System.Windows.Forms.Button markAll;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public string recipentId;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // GRUPA TEXTBOX'A DO WPISYWANIA
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        // GRUPA TEXTBOX'A Z WYNIKIEM DZIALANIA
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButton1.Checked = true;
+        }
+
+        // OPCJA SZYFRUJ
+        public void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            groupBox4.Visible = true;
+        }
+
+        // OPCJA ODSZYFRUJ
+        public void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            groupBox4.Visible = false;
+        }
+
+        // WYWOLANIE CMD
+        public void ExecuteCommand(string Command)
+        {
+            ProcessStartInfo ProcessInfo;
+            Process Process;
+
+            ProcessInfo = new ProcessStartInfo("cmd.exe", "/c " + Command);
+
+            ProcessInfo.CreateNoWindow = true;
+            ProcessInfo.UseShellExecute = true;
+            ProcessInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            Process = Process.Start(ProcessInfo);
+            Process.WaitForExit();
+        }
+
+        static public string EncodeTo64(string toEncode)
+
+        {
+
+            byte[] toEncodeAsBytes
+
+                  = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
+
+            string returnValue
+
+                  = System.Convert.ToBase64String(toEncodeAsBytes);
+
+            return returnValue;
+
+        }
+
+        static public string DecodeFrom64(string encodedData)
+
+        {
+
+            byte[] encodedDataAsBytes
+
+                = System.Convert.FromBase64String(encodedData);
+
+            string returnValue =
+
+               System.Text.ASCIIEncoding.ASCII.GetString(encodedDataAsBytes);
+
+            return returnValue;
+
+        }
+        // FUNKCJA SZYFRUJACA
+        public void Encrypt(string recipentId)
+        {
+            if (radioButton1.Checked == true)
+            {
+                string text;
+                string encodedText;
+                if (textBox1.TextLength == 0)
+                {
+                    MessageBox.Show("Wpisz wiadomość!");
+                }
+                else
+                {
+                    text = textBox1.Text;
+                    if (String.IsNullOrEmpty(recipentId))
+                    {
+                        MessageBox.Show("Wybierz klucze publiczne\ndla jakich szyfrować !");
+                    }
+                    else
+                    {
+                        ExecuteCommand("echo " + text + "|gpg --encrypt --armor --trust-model always" + recipentId + "|clip");
+                        text = Clipboard.GetText();
+                        encodedText = EncodeTo64(text);
+                        Clipboard.SetText(encodedText);
+                        //textBox2.Text = encodedText;
+                        textBox2.Text = Clipboard.GetText();
+                    }
+                }
+            }
+
+        }
+
+        // FUNKCJA DESZYFRUJACA
+        public void Decrypt()
+        {
+            string text;
+            text = textBox1.Text;
+            if (radioButton2.Checked == true) //odszyfrowanie
+            {
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\szyfr.txt";
+                string text1;
+                string decodedText;
+                if (textBox1.TextLength == 0)
+                {
+                    MessageBox.Show("Wpisz wiadomość!");
+                }
+                else
+                {
+                    text1 = textBox1.Text;
+                    decodedText = DecodeFrom64(text1);
+                    File.WriteAllText(path, decodedText);
+                    ExecuteCommand("gpg -d --try-all-secrets " + path + " |clip");
+                    textBox2.Text = Clipboard.GetText();
+                }
+            }
+
+        }
+
+        //PRZYCISK "URUCHOM NARZEDZIE"
+        public void button1_Click(object sender, EventArgs e)
+        {
+            //Łukasza
+            if (checkBox1.Checked)
+            {
+                recipentId = recipentId + " -r 79501F0F706A82C3C656C7691DBD19D3E45587CD";
+
+            }
+            //Gabrieli
+            if (checkBox2.Checked)
+            {
+                recipentId = recipentId + " -r 0FB871E54C210175B87742DAF6A562AE18695955";
+
+            }
+            //Pawła
+            if (checkBox3.Checked)
+            {
+                recipentId = recipentId + " -r 386DCEC744F9A62827F248C05CEA18D0255A858E";
+
+            }
+            //Kuby
+            if (checkBox4.Checked)
+            {
+                recipentId = recipentId + " -r 9781445CCAD4E844A29324B059214BDB762965E9";
+
+            }
+            //Mateusza
+            if (checkBox5.Checked)
+            {
+                recipentId = recipentId + " -r DF18EDFFA8C1087CFE12AC5B4BF62E5A0139F58E";
+
+            }
+            //Dastina
+            if (checkBox6.Checked)
+            {
+                recipentId = recipentId + " -r 6567844E3D0FF11418E94B9673636622ACF5CB01";
+
+            }
+
+
+            //wywołanie funkcji void odpowiedzialnych za działanie programu
+            if (radioButton1.Checked == true)
+            {
+                Encrypt(recipentId);
+            }
+            if (radioButton2.Checked == true)
+            {
+                Decrypt();
+            }
+
+
+        }
+
+        // CHECKBOXY Z KLUCZAMI
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        // GRUPA Z CHECKBOX'AMI KLUCZY
+        private void groupBox4_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        // USUWANIE PLIKÓW TYMCZASOWYCH, TWORZONYCH PODCZAS DZIALANIA APKI
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\szyfr.txt";
+
+            File.Delete(path);
+        }
+
+        // O NAS > TWORCY
+        private void twórcyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Koder: Paszkowski Dastin\n" +
+                "Menadżer projektu: Jakub Żurawski\n" +
+                "Tester: Paweł Czarnocki\n" +
+                "Research/Testerr: Mateusz Gałka\n");
+        }
+
+        // O NAS > STRONA WWW
+        private void stronaWWWToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://polchiffre.pl");
+        }
+
+        private void markAll_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in groupBox4.Controls)
+            {
+                if (c is CheckBox)
+                {
+                    CheckBox cb = (CheckBox)c;
+                    if (cb.Checked == false)
+                    {
+                        cb.Checked = true;
+                        markAll.Text = "Odznacz wszystkich";
+                    }
+                    else
+                    {
+                        cb.Checked = false;
+                        markAll.Text = "Zaznacz wszystkich";
+                    }
+                }
+            }
+        }
+    }
+}
